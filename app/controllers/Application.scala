eb4ee1c7c5feffb9ec8dfb3b2247b1bfaad70cc3
kazufumi_nishida <kazufumi_nishida@dwango.co.jp>
2013-07-01 15:10:28 +0900

    postされたデータを読めるようになった

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,15 +1,28 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
 
 object Application extends Controller {
+
+  val newAccountForm = Form(
+    tuple(
+      "username" -> text,
+      "password" -> text
+    )
+  )
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
 
   def new_account = Action {
-    Ok(views.html.new_account())
+    Ok(views.html.new_account(newAccountForm))
+  }
+
+  def post_new_account = Action { implicit request =>
+    val (username, password) = newAccountForm.bindFromRequest.get
+    Ok("hello, " + username)
   }
 }
